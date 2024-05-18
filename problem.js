7ed5906c220f82ab771cb7c586b6646bf68ecc7f
arafat717 <arafatjibon33@gmail.com>
2024-05-18 07:26:55 +0600

    solved 3 problems

--- problem.js
+++ problem.js
@@ -109,4 +109,87 @@ const AscendingCars = (cars) => {
   return car;
 };
 
-console.log(AscendingCars(carsArray));
+// console.log(AscendingCars(carsArray));
+
+// Write A Function That Searches An Array Of Objects For A Specific Person By Name. If Found, Modify Their Age Property. Print The Updated Array.//
+
+const persons2 = [
+  {
+    name: "Arafat",
+    age: 19,
+    gender: "male",
+  },
+  {
+    name: "Rabia",
+    age: 19,
+    gender: "female",
+  },
+  {
+    name: "Monika",
+    age: 19,
+    gender: "female",
+  },
+  {
+    name: "Sumaiya",
+    age: 19,
+    gender: "female",
+  },
+  {
+    name: "Mahdi",
+    age: 19,
+    gender: "male",
+  },
+];
+
+const modifiedAge = (items, newage, persomName) => {
+  const person = items.find((item) => item.name === persomName);
+  if (person) {
+    person.age = newage;
+  }
+  return items;
+};
+
+// console.log(modifiedAge(persons2, 5, "Mahdi"));
+
+// Create An Array Of Numbers. Write A Function That Uses The Reduce Method To Calculate The Sum Of All Even Numbers In The Array.
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12];
+
+const sumOfArray = (array) => {
+  const sum = array.reduce((acc, fin) => {
+    return acc + fin;
+  }, 0);
+  return sum;
+};
+
+// console.log(sumOfArray(numbers));
+
+// Write A Function That Determines Whether A Given Year Is A Leap Year.//
+
+const leapYear = (year) => {
+  const leap = year % 4;
+  if (leap) {
+    console.log(year, "is a leap year");
+  } else {
+    console.log(year, "is not a leap year");
+  }
+};
+
+// leapYear(2024);
+
+// Write A Function That Counts The Number Of Vowels In A Given String.//
+
+const CountVowels = (string) => {
+  string = string.toLowerCase();
+  const vowel = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (vowel.includes(string[i])) {
+      count++;
+    }
+  }
+  return count;
+};
+
+const word = "the name of my country is bangladesh";
+console.log(CountVowels(word));
